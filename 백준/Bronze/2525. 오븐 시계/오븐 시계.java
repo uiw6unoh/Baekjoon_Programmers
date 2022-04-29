f06d3355308b1f0489667541da1cb08b91fb2225
Young Min <95083025+uiw6unoh@users.noreply.github.com>
2022-04-29 22:19:05 +0900

    [Bronze IV] Title: 오븐 시계, Time: 228 ms, Memory: 18388 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int hour = sc.nextInt();
+		int minute = sc.nextInt();
+		int time = sc.nextInt();
+		
+		hour += time / 60;
+		minute += time % 60;
+		
+		if (minute >= 60) {
+			hour += 1;
+			minute -= 60;
+		} if (hour >= 24) {
+			hour -=24;
+		}
+		System.out.println(hour + " " + minute);
+		
+		sc.close();
+	}
+}
