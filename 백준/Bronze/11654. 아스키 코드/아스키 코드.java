c1bff6a58280355a17c1a5b0f12aefea7743d960
Young Min <95083025+uiw6unoh@users.noreply.github.com>
2022-05-11 20:44:56 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17548 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String a = sc.next();
+
+		System.out.println(a.charAt(0)+0);
+		
+		sc.close();
+	}
+}
