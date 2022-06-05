cd6ca9ca07391bd87334edfcdc48536095e45a32
Young Min <95083025+uiw6unoh@users.noreply.github.com>
2022-06-05 19:37:10 +0900

    [Bronze II] Title: 상수, Time: 236 ms, Memory: 18500 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int a = rn(in.nextInt());
+        int b = rn(in.nextInt());
+
+        System.out.println((a>b)? a:b);
+    }
+
+    static int rn(int num){
+        String number = String.valueOf(num);
+        String temp = "0";
+        for (int i = 3; i > 0 ; i--) {
+            temp += number.charAt(i-1);
+        }
+        return Integer.parseInt(temp);
+    }
+}
